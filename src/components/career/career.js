f98ac4e3262a48136b7247dc83d116e7c2697474
Freddy Rangel <frederick.rangel@gmail.com>
2018-12-26 18:49:11 -0700

    Finished Career Section

--- src/components/career/career.js
+++ src/components/career/career.js
@@ -3,7 +3,6 @@ import styled                                     from 'styled-components';
 import SR                                         from 'scrollreveal';
 import { Heading, Section, media, mixins, theme } from 'styles';
 import { srConfig }                               from 'utils';
-import Tabs                                       from './tabs';
 
 const { colors, fonts, fontSizes } = theme;
 
@@ -26,6 +25,10 @@ const careerData = [
     fromTo: 'June 2015 - March 2017',
     title: 'Senior Front End Engineer',
     accomplishments: [
+      'Improved JavaScript runtime performance by a factor of 15',
+      'Reduced initial browser load-time from 4.5 seconds to 200 milliseconds',
+      'Helped build HelloWorks, a new signature product based on an event-driven architecture',
+      'Gave talks and workshops for developer conferences all over the US'
     ]
   },
   {
@@ -33,6 +36,10 @@ const careerData = [
     fromTo: 'August 2014 - June 2015',
     title: 'Software Developer',
     accomplishments: [
+      'Refactored large legacy enterprise systems using TDD and increased test coverage from 0 to 85 percent while adding new features',
+      'Architected and built backend JSON APIs for web, hybrid, and native applications',
+      'Built a social networking application (SkyRise) connecting office tenants into online communities',
+      'Taught other developers how to write and maintain test suites'
     ]
   },
   {
@@ -40,6 +47,8 @@ const careerData = [
     fromTo: 'December 2013 - July 2014',
     title: 'Software Developer',
     accomplishments: [
+      'Designed and build a modular medical appointment scheduling app using Ruby on Rails',
+      'Extracted major features into reusable components via Rails engines to build an e-commerce platform and a direct sales support application'
     ]
   }
 ];
@@ -114,6 +123,136 @@ const JobDetails = styled.h5`
     width: 15px;
   }
 `;
+const TabsContainer = styled.div`
+  display: flex;
+  align-items: flex-start;
+  position: relative;
+  ${media.thone`display: block;`};
+`;
+
+const TabItems = styled.div`
+  display: block;
+  position: relative;
+  width: max-content;
+  z-index: 3;
+  ${media.thone`
+    display: flex;
+    margin-bottom: 30px;
+    width: 100%;
+    overflow-x: scroll;
+  `};
+`;
+
+const TabItem = styled.button`
+  ${mixins.link};
+  display: flex;
+  align-items: center;
+  width: 100%;
+  background-color: transparent;
+  height: ${theme.tabHeight}px;
+  padding: 0 20px 2px;
+  transition: ${theme.transition};
+  border-left: 2px solid ${colors.darkGrey};
+  text-align: left;
+  white-space: nowrap;
+  font-family: ${fonts.SFMono};
+  font-size: ${fontSizes.smallish};
+  color: ${props => (props.isActive ? colors.green : colors.lightGrey)};
+  ${media.tablet`padding: 0 15px 2px;`};
+  ${media.thone`
+    ${mixins.flexCenter};
+    padding: 0 15px;
+    text-align: center;
+    border-left: 0;
+    border-bottom: 2px solid ${colors.darkGrey};
+    min-width: 120px;
+  `};
+  &:hover,
+  &:focus {
+    background-color: ${colors.lightNavy};
+  }
+`;
+
+const Highlighter = styled.span`
+  display: block;
+  background: ${colors.green};
+  width: 2px;
+  height: ${theme.tabHeight}px;
+  border-radius: ${theme.borderRadius};
+  position: absolute;
+  top: 0;
+  left: 0;
+  transition: ${theme.transition};
+  transition-delay: 0.1s;
+  z-index: 10;
+  transform: translateY(
+    ${props => (props.activeTabId > 0 ? props.activeTabId * theme.tabHeight : 0)}px
+  );
+  ${media.thone`
+    width: 100%;
+    max-width: ${theme.tabWidth}px;
+    height: 2px;
+    top: auto;
+    bottom: 0;
+    transform: translateX(
+      ${props => (props.activeTabId > 0 ? props.activeTabId * theme.tabWidth: 0)}px
+    );
+  `};
+`;
+
+function Tabs({ careerData = [], activeTabId, handleClick, isActive }) {
+
+  const tabItems = careerData.map((data, i) => {
+    const isTabActive = isActive(i)
+    const tabItemProps = {
+      key: i,
+      onClick: e => handleClick(i),
+      isActive: isTabActive,
+      role: 'tab',
+      'aria-selected': isTabActive ? 'true' : 'false',
+      'aria-controls': `tabs${i}`,
+      id: `tabs${i}`,
+      tabIndex: isTabActive ? '0' : '-1'
+    };
+
+    return <TabItem { ...tabItemProps }>{ data.company }</TabItem>;
+  });
+
+  const content = careerData && careerData.map((data, i) => {
+    const tabContentProps = {
+      key: i,
+      isActive: isActive(i),
+      id: `job${i}`,
+      role: 'tabpanel',
+      tabIndex: 0,
+      ariaLabelledby: `job${i}`,
+      ariaHidden: !isActive(i),
+    };
+    return <TabContent { ...tabContentProps }>
+      <JobTitle>
+        <span>{data.title}</span>
+        <Company>
+          &nbsp;@&nbsp;
+          <a href={ data.url } target="_blank" rel="nofollo noopener noreferrer">{ data.company }</a>
+        </Company>
+      </JobTitle>
+      <JobDetails><span>{ data.fromTo }</span></JobDetails>
+      <ul>
+        { data.accomplishments && data.accomplishments.map((accomplishment, i) => <li key={i}>{accomplishment}</li>) }
+      </ul>
+    </TabContent>;
+  });
+
+  return <TabsContainer>
+    <TabItems role="tabitems">
+      { tabItems }
+      <Highlighter activeTabId={ activeTabId } />
+    </TabItems>
+    <ContentContainer>
+      { content }
+    </ContentContainer>
+  </TabsContainer>;
+}
 
 export default class Career extends Component {
 
@@ -125,46 +264,15 @@ export default class Career extends Component {
     SR().reveal(this.career, srConfig());
   }
 
-  isActive = id => {
-    console.log('this is the current active tab', this.state.activeTabId);
-    console.log('this is the current tab', id);
-    return this.state.activeTabId === id;
-  };
+  isActive = id => this.state.activeTabId === id;
 
   setActiveTab = (activeTabId) => this.setState({ activeTabId });
 
   render() {
-    const content = careerData && careerData.map((data, i) => {
-      const tabContentProps = {
-        key: i,
-        isActive: this.isActive(i),
-        id: `job${i}`,
-        role: 'tabpanel',
-        tabIndex: 0,
-        ariaLabelledby: `job${i}`,
-        ariaHidden: !this.isActive(i),
-      };
-      return <TabContent { ...tabContentProps }>
-        <JobTitle>
-          <span>{data.title}</span>
-          <Company>
-            &nbsp;@&nbsp;
-            <a href={ data.url } target="_blank" rel="nofollo noopener noreferrer">{ data.company }</a>
-          </Company>
-        </JobTitle>
-        <JobDetails><span>{ data.fromTo }</span></JobDetails>
-        <ul>
-          { data.accomplishments && data.accomplishments.map((accomplishment, i) => <li key={i}>{accomplishment}</li>) }
-        </ul>
-      </TabContent>;
-    });
 
     return <CareerContainer id="career" ref={el => (this.career = el)}>
       <Heading>Work History</Heading>
       <Tabs { ...this.state } { ...this.props } careerData={ careerData } handleClick={ this.setActiveTab } isActive={ this.isActive }/>
-      <ContentContainer>
-        { content }
-      </ContentContainer>
     </CareerContainer>;
   }
 }
