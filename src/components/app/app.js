1d4d12f35f89daf8731549fce7dce1011ec54ef3
Freddy Rangel <frederick.rangel@gmail.com>
2018-12-27 17:05:27 -0700

    Hero Section

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,18 +2,20 @@ import React                              from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import styled                             from 'styled-components';
 import Layout                             from 'components/layout';
+import Hero                               from 'components/hero';
 import About                              from 'components/about';
 import Career                             from 'components/career';
 import { mixins, Main }                   from 'styles';
 
 const MainContainer = styled(Main)`
-    ${mixins.sidePadding};
-    counter-reset: section;
+  ${mixins.sidePadding};
+  counter-reset: section;
 `;
 
 function FullLayout(props) {
   return <Layout { ...props }>
     <MainContainer { ...props }>
+      <Hero { ...props }/>
       <About { ...props }/>
       <Career { ...props }/>
     </MainContainer>
---
+++ src/components/hero/hero.js
@@ -0,0 +1,112 @@
+import React, { Component }               from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import siteData                           from 'content/site-data';
+import styled                             from 'styled-components';
+import { theme, mixins, media, Section }  from 'styles';
+
+const { colors, fontSizes, fonts } = theme;
+const { email } = siteData;
+
+const HeroContainer = styled(Section)`
+  ${mixins.flexCenter};
+  flex-direction: column;
+  align-items: flex-start;
+  min-height: 100vh;
+  ${media.tablet`padding-top: 150px;`};
+  div {
+    width: 100%;
+  }
+`;
+const Hi = styled.h1`
+  color: ${colors.green};
+  margin: 0 0 20px 3px;
+  font-size: ${fontSizes.medium};
+  font-family: ${fonts.SFMono};
+  font-weight: normal;
+  ${media.desktop`font-size: ${fontSizes.small};`};
+  ${media.tablet`font-size: ${fontSizes.smallish};`};
+`;
+const Name = styled.h2`
+  font-size: 80px;
+  line-height: 1.1;
+  margin: 0;
+  ${media.desktop`font-size: 70px;`};
+  ${media.tablet`font-size: 60px;`};
+  ${media.phablet`font-size: 50px;`};
+  ${media.phone`font-size: 40px;`};
+`;
+const Subtitle = styled.h3`
+  font-size: 80px;
+  line-height: 1.1;
+  color: ${colors.slate};
+  ${media.desktop`font-size: 70px;`};
+  ${media.tablet`font-size: 60px;`};
+  ${media.phablet`font-size: 50px;`};
+  ${media.phone`font-size: 40px;`};
+`;
+const Blurb = styled.div`
+  margin-top: 25px;
+  width: 50%;
+  max-width: 500px;
+  a {
+    ${mixins.inlineLink};
+  }
+`;
+const EmailLink = styled.a`
+  ${mixins.bigButton};
+  font-size: ${fontSizes.smallish};
+  margin-top: 50px;
+`;
+
+class Hero extends Component {
+
+  state = {
+    isMounted: false,
+  };
+
+  componentDidMount() {
+    setTimeout(() => this.setState({ isMounted: true }), 1000);
+  }
+
+  componentWillUnmount() {
+    this.setState({ isMounted: false });
+  }
+
+  render() {
+    const { data } = this.props;
+    const { isMounted } = this.state;
+
+    const one = () => <Hi style={{ transitionDelay: '100ms' }}>Hi, my name is</Hi>;
+    const two = () => <Name style={{ transitionDelay: '200ms' }}>Freddy Rangel.</Name>;
+    const three = () => (
+      <Subtitle style={{ transitionDelay: '300ms' }}>I sling code for a living.</Subtitle>
+    );
+    const four = () => (
+      <Blurb style={{ transitionDelay: '400ms' }}>
+        <p>I'm a software engineer in the San Francisco Bay Area specializing in building real-world, business critical applications.</p>
+      </Blurb>
+    );
+    const five = () => (
+      <div style={{ transitionDelay: '500ms' }}>
+        <EmailLink href={`mailto:${email}`}>Get In Touch</EmailLink>
+      </div>
+    );
+
+    const items = [one, two, three, four, five];
+
+    return (
+      <HeroContainer>
+        <TransitionGroup>
+          {isMounted &&
+            items.map((item, i) => (
+              <CSSTransition key={i} classNames="fadeup" timeout={3000}>
+                {item}
+              </CSSTransition>
+            ))}
+        </TransitionGroup>
+      </HeroContainer>
+    );
+  }
+}
+
+export default Hero;
